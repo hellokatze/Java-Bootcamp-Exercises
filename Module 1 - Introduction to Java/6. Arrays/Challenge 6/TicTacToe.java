f4e6016ea1be3cb4a921a333f3f35a562a172ead
Katharine Lin <katharine.exe@gmail.com>
2023-07-18 09:58:27 +0200

    create board, printBoard() function

--- Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
+++ Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
@@ -4,22 +4,27 @@ public class TicTacToe {
 
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
             System.out.println("\nLet's play tic tac toe");
 
             //Task 1: Create an array with three rows of '_' characters.
+            char[][] board = {
+              {'_', '_', '_'},
+              {'_', '_', '_'},
+              {'_', '_', '_'},
+            };
 
             //Task 2: Call the function printBoard();
+            printBoard(board);
 
-             
               /*
               {  Task 3: Loop through turns.
 
                   if (X) turn {
-                     Task 4: call askUser(). 
+                     Task 4: call askUser().
                      Task 5: populate the board using askUser's return value.
                   } else {
-                      Task 4: call askUser(). 
+                      Task 4: call askUser().
                       Task 5: populate the board using askUser's return value. Then, print it.
 
                   }
@@ -31,7 +36,7 @@ public class TicTacToe {
                      print: O wins and break the loop
                   }
 
-              } 
+              }
               */
 
             scan.close();
@@ -41,32 +46,44 @@ public class TicTacToe {
     /** Task 2 - Write a function that prints the board.
      * Function name - printBoard()
      * @param board (char[][])
-     * 
+     *
      * Inside the function:
      *   1. print a new line.
      *   2. print the board.
-     *      • separate each row by two lines. 
+     *      • separate each row by two lines.
      *      • each row precedes a tab of space
      *      • each character in the grid has one space from the other character
-     */        
+     */
+
+     public static void printBoard(char[][] arr) {
+        System.out.print("\n");
+        for (int i = 0; i < arr.length; i++) {
+          System.out.print("\t");
+          for (int j = 0; j < arr[i].length; j++) {
+            System.out.print(arr[i][j] + " ");
+          }
+          System.out.print("\n");
+          System.out.print("\n");
+        }
+     }
 
    /** Task 4 - Write a function that lets the user choose a spot
      * Function name – askUser
      * @param board (char[][] board)
      * @return spot (int[])
-     * 
+     *
      * Inside the function
-     *   1. Asks the user: - pick a row and column number: 
+     *   1. Asks the user: - pick a row and column number:
      *   2. Check if the spot is taken. If so, let the user choose again.
      *   3. Return the row and column in an int[] array.
-     * 
+     *
      */
 
     /** Task 6 - Write a function that determines the winner
-     * Function name - checkWin 
+     * Function name - checkWin
      * @param board (char[][])
      * @return count (int)
-     * 
+     *
      * Inside the function:
      *   1. Make a count variable that starts at 0.
      *   2. Check every row for a straight X or straight O (Task 7).
